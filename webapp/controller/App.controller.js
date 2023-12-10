af789bfde4497355a0fd92ca79211c104165c234
sumanlucifer <suman.lucifer94@gmail.com>
2023-12-10 10:05:47 +0000

    test

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -5,6 +5,7 @@ sap.ui.define([
 
 	return Controller.extend("com.swcc.finance.SSP_Finance.controller.App", {
 		onInit: function () {
+			console.log("test");
 
 			this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
 		}
